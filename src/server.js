e1156c90606dfc3685f39b11b275c2d7d0a2f4f2
douglasgvr <douglasfarmaangra@gmail.com>
2024-09-25 15:25:25 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -2,18 +2,34 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
+
   if (method === "GET" && url === "/users") {
-    return response.end(JSON.stringify(users));
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
     users.push({
       id: 1,
-      name: "Douglas",
-      email: "douglas@gmail.com",
+      name,
+      email,
     });
   }
 
