30d34fd54d0c50b33c9ff5a5d7d86eee42642a51
douglasgvr <douglasfarmaangra@gmail.com>
2024-09-25 14:43:20 -0300

    consumindo ums streams completa

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredStream extends Readable {
     const i = this.index++;
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null);
       } else {
         const buf = Buffer.from(String(i));
@@ -17,3 +17,14 @@ class OneToHundredStream extends Readable {
     }, 1000);
   }
 }
+
+fetch("http://localhost:3334", {
+  method: "POST",
+  body: new OneToHundredStream(),
+})
+  .then((res) => {
+    return res.text();
+  })
+  .then((data) => {
+    console.log(data);
+  });
